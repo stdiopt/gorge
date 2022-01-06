99f6314d7be6223c8ad3cee52ff34a4117c647e5
luisf <stdiopt@gmail.com>
2022-01-06 00:16:04 -0300

    Fix unlit additive shader

--- static/src/shaders/unlit_additive.glsl
+++ static/src/shaders/unlit_additive.glsl
@@ -1,6 +1,6 @@
 #version 300 es
 
-precision mediump float;
+precision highp float;
 
 layout(std140) uniform Camera {
 	mat4 VP;
@@ -9,23 +9,27 @@ layout(std140) uniform Camera {
 };
 
 #ifdef VERT_SRC
-in vec3 a_Position;
-in vec3 a_Normal;
-in vec2 a_UV1;
-in vec4 a_InstanceColor;
-in mat4 a_Transform;
-in mat4 a_NormalTransform;
+layout (location = 0) in vec3 a_Position;
+layout (location = 1) in vec3 a_Normal;
+layout (location = 2) in vec2 a_UV1;
+
+// Instance stuff
+layout (location = 3) in vec4 a_InstanceColor;
+layout (location = 4) in mat4 a_Transform;
+// location = 5 aTransform
+// location = 6 aTransform
+// location = 7 aTransform
+layout (location = 8) in mat4 a_NormalTransform;
 
 out vec4 ColorV;
-out vec3 FragPos;
+//out vec3 FragPos;
 out vec3 Normal;
 out vec2 TexCoords;
 
-
 void main() {
 	ColorV = a_InstanceColor;
-	FragPos = vec3(a_Transform * vec4(a_Position, 1.0));
-	Normal = mat3(a_Transform) * a_Normal;
+	vec3 FragPos = vec3(a_Transform * vec4(a_Position, 1.0));
+	Normal = normalize(vec3(a_NormalTransform * vec4(a_Normal, 0.0)));
 	TexCoords = a_UV1;
 
 	gl_Position = VP * vec4(FragPos, 1.0);
@@ -43,10 +47,14 @@ uniform sampler2D albedoMap;
 
 void main() {
 	vec4 tex = texture(albedoMap, TexCoords);
-	if (tex.a <= 0.01) {
+	
+	float alpha = tex.a * ColorV.a;
+	vec3 color = tex.rgb * ColorV.rgb;
+
+	if (alpha <= 0.001) {
 		discard;
 	}
-	tex *= ColorV;
-	FragColor = vec4(tex.rgb, tex.a);
+	//tex *= ColorV;
+	FragColor = vec4(color * alpha , alpha);
 }
 #endif
