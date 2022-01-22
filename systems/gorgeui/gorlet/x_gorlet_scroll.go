540595a788a0f46ee8261d1585260b476f39a8f2
luisf <stdiopt@gmail.com>
2022-01-21 23:05:10 -0300

    Scroll anchor

--- systems/gorgeui/gorlet/x_gorlet_scroll.go
+++ systems/gorgeui/gorlet/x_gorlet_scroll.go
@@ -31,8 +31,7 @@ func Scroll() Func {
 			b.UseRect(0, 0, scroll[1].size, scroll[0].size)
 			scrollable = b.BeginMask()
 			{
-				// b.UseAnchor(0, 0, 1, 1)
-				b.UseAnchor(0)
+				b.UseAnchor(0, 0, 1, 1)
 				container = b.BeginContainer()
 				b.ClientArea()
 				b.EndContainer()
